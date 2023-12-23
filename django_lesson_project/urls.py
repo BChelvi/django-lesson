d9ba55255ecefc181a199d0c691b4e8386757cf0
Cédric Jannot <cedric.skiza@gmail.com>
2023-12-23 07:06:43 +0100

    Urls for zoo

--- django_lesson_project/urls.py
+++ django_lesson_project/urls.py
@@ -17,9 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from zoo.views import say_hello, say_hello_with_template, animal_list
+
 admin.site.site_header = "Projet Django Lesson"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('area_manager/', include('area_manager.urls'))
+
+    # Urls from the area_manager app
+    path('area_manager/', include('area_manager.urls')),
+
+    # Urls from the zoo app
+    path('zoo/hello/', say_hello),
+    path('zoo/hello_template/', say_hello_with_template),
+    path('zoo/animal_list/', animal_list),
 ]
